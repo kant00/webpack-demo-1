9dc0ab400ddfa1cbba1909efa0fba2b2cdc33461
kant0 <1195510518@qq.com>
2020-09-18 10:13:57 +0800

    final

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import x from "./x.js";
-console.log(x);
+import jpg from "./assets/1.jpg";
+const div = document.getElementById("app");
+div.innerHTML = `<img src='${jpg}'>`;
+const button = document.createElement("button");
+button.innerText = "懒加载";
+div.appendChild(button);
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {
+      console.log("加载错误");
+    }
+  );
+};
